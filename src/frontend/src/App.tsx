210c11342cb7e1dc73a6fe96cb4ba847151ed900
Ariel Merino <arielmerino@ciencias.unam.mx>
2025-03-03 11:11:17 -0600

    feat(frontend): add Tailwind CSS configuration and integrate into project

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -7,35 +7,6 @@ interface DataResponse {
   message: string;
 }
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
 function App() {
   const [data, setData] = useState<DataResponse | null>(null);
 
@@ -47,8 +18,8 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>React + Django con TS y SWC</h1>
+    <div className='w-full h-full flex-col'>
+      <h1 className='text-4xl font-bold'>React + Django con TS y SWC</h1>
       {data ? <p>{data.message}</p> : <p>Cargando...</p>}
     </div>
   );
