b50e1a8f592fd35da1b2c14e298ace177999188c
laudima <lr255029@teradata.com>
2025-03-13 15:58:57 -0600

    add student

--- src/backend/api/admin.py
+++ src/backend/api/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models.scholarship import Scholarship
 from .models.student import Student
+from .models.organization import Organization
+from .models.organization import Membership
+
 # Register your models here.
-# admin.site.register(Scholarship)
+
 admin.site.register(Student)
+# admin.site.register(Scholarship)
+# admin.site.register(Organization)
+# admin.site.register(Membership)
