dd19c5de007e1dfcecac75ac96c4e0c6aa61679a
laudima <lr255029@teradata.com>
2025-03-12 23:08:15 -0600

    student

--- src/backend/api/models/student.py
+++ src/backend/api/models/student.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth.models import User
 
 class Student(models.Model):
     search = models.CharField(max_length=255)
-    intereses = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=15, null= True, unique=True)
+    interests = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15, blank=True, unique=True)
     birthday = models.DateField(blank=True, null=True)
-    user = models.OneToOneField('User', on_delete=models.CASCADE, related_name='student')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student')
