ae64dae4dd70b05cdcd11f15dd11acc43180ebbd
Laura Dimayuga <laudima@ciencias.unam.mx>
2025-05-29 13:59:06 -0600

    not worked

--- src/frontend/src/components/Opportunities/EditOpportunityForm.tsx
+++ src/frontend/src/components/Opportunities/EditOpportunityForm.tsx
@@ -241,10 +241,7 @@ const EditOpportunityForm: React.FC<EditOpportunityFormProps> = ({ opportunity, 
             }
             
             // Append organization if it exists
-            if (data.organization_id) {
-                if (data.organization_id === "0") {
-                    formData.append('organization_id', null as any); // Use null if no organization is selected
-                }
+            if (data.organization_id && data.organization_id !== "0") {
                 formData.append('organization_id', data.organization_id.toString());
             }
 
